84ec4fa911a0000995155363705b6ffdbed02850
JuanGuzmanH <juand.guzmanh@utadeo.edu.co>
2021-05-08 16:12:21 -0500

    Segundo

--- src/Pedidos.js
+++ src/Pedidos.js
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './css/Estilo.css';
 
 export const Pedidos = () => {
     return (
         <div>
-            
+             <div className="titulo">
+                <Link to="/">
+                    <img src="https://i.imgur.com/h3FiSQx.png" alt="titulo"></img>
+                </Link>
+            </div>
         </div>
     )
 }
